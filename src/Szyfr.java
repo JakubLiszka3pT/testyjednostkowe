644dc1385ce999af4bec554dd2cc29621050a696
JakubLiszka3pT <jakub.liszka@uczen.mechaniktg.pl>
2026-04-10 10:20:31 +0200

    Testy poprawnie

--- src/Szyfr.java
+++ src/Szyfr.java
@@ -1,6 +1,7 @@
-public class Szyfry {
+public class Szyfr {
     public static String szyfrujPrzestawieniowo(String slowoDoSzyfrowania){
         String zaszyfrowaneslowo = "";
         return zaszyfrowaneslowo;
+        // prawy-> generate -> Test , testy powinny znajdowac sie w osobnym katalogu
     }
 }
--- src/SzyfrTest.java
+++ src/SzyfrTest.java
@@ -1,4 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 class SzyfrTest {
-  
+
+    @Test // metoda testująca
+    void szyfrujPrzestawieniowoJezeliPusteSlowo(){
+        Assertions.assertEquals("", Szyfr.szyfrujPrzestawieniowo(""));
+    }
+    @Test
+    void szyfrujPrzestawieniowoJezeliParzystaDlugoscSlowa(){
+        Assertions.assertEquals("atam", Szyfr.szyfrujPrzestawieniowo("tama"));
+    }
+    @Test
+    void szyfrujPrzestawieniowoJezeliJednaLitera(){
+        Assertions.assertEquals("z", Szyfr.szyfrujPrzestawieniowo("z"));
+    }
+    @Test
+    void szyfrujPrzestawieniowoJezeliNieparzystaDlugoscSlowa(){
+        Assertions.assertEquals("odemk", Szyfr.szyfrujPrzestawieniowo("domek"));
+    }
 }
+// wartosc wejscie-> otrzymana i oczekiwana
